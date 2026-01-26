101fc92e4c8d2e4c49c753757f7100a1cf1fbaa5
NamLe179 <ldnam179@gmail.com>
2026-01-26 11:05:05 +0700

    bổ sung thư viện cho orbat mapper

---
+++ declarations.d.ts
@@ -0,0 +1,13 @@
+// Các thư viện JS chưa có @types chính thức
+declare module "formatcoords";
+declare module "mgrs";
+
+// OpenLayers Extensions (ol-ext)
+// Nếu bạn chưa tạo file định nghĩa chi tiết cho DayNight ở bước trước,
+// dòng này sẽ giúp fix lỗi "implicitly has an 'any' type" nhanh chóng.
+declare module "ol-ext/layer/GeoImage";
+declare module "ol-ext/source/GeoImage";
+declare module "ol-ext/interaction/Transform";
+declare module "ol-ext/source/DayNight";
+
+declare module "@orbat-mapper/convert-symbology";
